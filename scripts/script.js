33acc99e90c610700826d09a6b27e98b568b8fe1
katya n <31803873+semilunar@users.noreply.github.com>
2018-05-26 22:53:58 +0300

    ыъ ъы
    ыъ ъы

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
   // var $draggable = $('.draggable').draggabilly();
 
   $('.ja').on('click', function() {
@@ -7,15 +8,20 @@ $(document).ready(function(){
     $('.t').hide()
     $('.i').hide()
     $('.e').html('Е ');
+    $('.me').show();
   });
 
   $('.b').on('hover', function() {
     $(this).html('B');
   });
 
-  $('.other').on('click', function() {
-    $('.post').toggleClass('gone');
+  $('.nothing').on('click', function() {
+    $(".pictures").hide();
+    $(".article").hide();
+    document.body.style.color = '#000';
   });
+
+
 
 
 });
