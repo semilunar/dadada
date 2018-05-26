710e9e24bc8dd251bf1d8f9681eec1f390460949
katya n <31803873+semilunar@users.noreply.github.com>
2018-05-26 22:58:17 +0300

    ъъ ъъ
    ъъ ъъ

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-  // var $draggable = $('.draggable').draggabilly();
+  var $draggable = $('.draggable').draggabilly();
 
   $('.ja').on('click', function() {
     $('.ja').hide()
@@ -20,6 +20,24 @@ $(document).ready(function(){
     $(".article").hide();
     document.body.style.color = '#000';
   });
+
+
+  $(".pictures > div").throwable({
+                      containment:[0,0,1400,780],
+                      drag:true,
+                      gravity:{x:1,y:1},
+                      impulse:{
+                          f:152,
+                          p:{x:1,y:1}
+                      },
+                      shape:"circle",
+                      autostart:true,
+                      bounce:9999,
+                      damping:1,
+                      areaDetection:[[0,0,300,300]],
+                      collisionDetection: true
+                  });
+
 
 
 
